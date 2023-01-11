fa1c18e9fb79a3c70b1cea5768e0c0e55192bb98
chankruze <chankruze@gmail.com>
2023-01-11 21:00:09 +0530

    minimal working tcp server
    Signed-off-by: chankruze <chankruze@gmail.com>

---
+++ app/src/main/java/app/bilar/tcpserver/MainActivity.kt
@@ -0,0 +1,25 @@
+package app.bilar.tcpserver
+
+import android.content.Intent
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import app.bilar.tcpserver.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        // start the foreground TCP server service
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            startForegroundService(Intent(applicationContext, TcpServerService::class.java))
+        } else {
+            startService(Intent(applicationContext, TcpServerService::class.java))
+        }
+    }
+}
